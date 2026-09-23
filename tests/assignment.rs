29b01ba91eb80815f1ba06684649d736175725ed
narsibhati-dev <narsibhati31102@gmail.com>
2026-09-24 01:25:27 +0530

    feat: build transfer checked with fee

--- tests/assignment.rs
+++ tests/assignment.rs
@@ -98,3 +98,30 @@ fn calculate_transfer_fee_from_current_epoch() {
     assert_eq!(fee.calculate_fee(100), Some(1));
 }
 
+
+#[test]
+fn builds_transfer_checked_with_fee() {
+    let token_program = spl_token_2022::id();
+    let source = solana_pubkey::Pubkey::new_unique();
+    let mint = solana_pubkey::Pubkey::new_unique();
+    let destination = solana_pubkey::Pubkey::new_unique();
+    let authority = solana_pubkey::Pubkey::new_unique();
+
+    let ix = token22_remittance::token22::transfer_with_fee(
+        &token_program,
+        &source,
+        &mint,
+        &destination,
+        &authority,
+        1_000_000,
+        6,
+        10_000,
+    );
+
+    assert_eq!(ix.program_id, token_program);
+    assert_eq!(ix.accounts.len(), 4);
+    assert_eq!(ix.accounts[0].pubkey, source);
+    assert_eq!(ix.accounts[1].pubkey, mint);
+    assert_eq!(ix.accounts[2].pubkey, destination);
+    assert_eq!(ix.accounts[3].pubkey, authority);
+}
